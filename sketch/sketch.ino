1d52f5127c9843baf993d9114db7e05dcc8998e5
neolao <contact@neolao.com>
2022-03-12 14:11:28 +0100

    up

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,23 +5,31 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
-#define BUTTON_CLK_PIN 4
-#define BUTTON_DT_PIN 3
-#define BUTTON_SW_PIN 2
+#define BUTTON_CLK_PIN 2
+#define BUTTON_DT_PIN 4
+#define BUTTON_SW_PIN 3
+#define MOISTURE_SENSOR_1_PIN A0
+#define MOISTURE_SENSOR_2_PIN A1
+#define MOISTURE_SENSOR_3_PIN A2
 //#define ENCODER_DO_NOT_USE_INTERRUPTS
+
+#define MIN_MOISTURE_VALUE 300
+#define MAX_MOISTURE_VALUE 600
+
 
 #define MAX_VOLT 4.2
 #define MIN_VOLT 3.2
 #define BATTERY_GAUGE_WIDTH 10
 
-#define DELAY_POWER_SAVER 20000
+#define DELAY_POWER_SAVER 60000
+
+#define PLANT_COUNT 8
 
 
 Adafruit_SSD1306 screen(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 Encoder rotaryButton(BUTTON_CLK_PIN, BUTTON_DT_PIN);
-long rotaryButtonValue = 0;
-long counter = 0;
+long rotaryPosition = 0;
 long lastActivityTime;
 
 enum Page
@@ -31,38 +39,47 @@ enum Page
 };
 Page currentPage = PAGE_HOME;
 uint8_t homePageMenuIndex = 0;
+uint8_t homePageMenuOffset = 0;
 uint8_t plantPageMenuIndex = 0;
+uint8_t plantPageMenuOffset = 0;
+char *plantPageMenu[] = {
+  "Retour",
+  "Arroser",
+  "Interval",
+  "Seuil"
+};
+uint8_t plantPageMenuSize = 4;
 
 void setup() {
   /*
-  Serial.begin(2000000);
-  Serial.println(F("Initialize System"));
+    Serial.begin(2000000);
+    Serial.println(F("Initialize System"));
   //*/
-  
+
   setupPowerSaver();
   setupScreen();
   setupRotaryButton();
+  setupMoistureSensors();
 }
 
 void loop() {
   checkPowerSaver();
 
-  long newPosition = rotaryButton.read();
-  if (newPosition != rotaryButtonValue) {
-    rotaryButtonValue = newPosition;
+  uint8_t newRotaryPosition = rotaryButton.read() / 4;
+  if (newRotaryPosition != rotaryPosition) {
+    if (newRotaryPosition > rotaryPosition) {
+      onNext();
+    } else {
+      onPrevious();
+    }
+    rotaryPosition = newRotaryPosition;
   }
 
 
   if (digitalRead(BUTTON_SW_PIN) == LOW) {
-    wakeUp();
-    rotaryButtonValue = 0;
+    onClick();
+    delay(100);
   }
-
-
-  rotaryButtonValue = (rotaryButtonValue / 4) % 20;
-
-  
-  counter++;
 
   updateScreen();
 }
@@ -87,7 +104,7 @@ void setupScreen() {
   screen.clearDisplay();
   screen.setTextWrap(false);
 
-  screen.setTextSize(2);
+  screen.setTextSize(1);
   screen.setTextColor(WHITE);
   screen.display();
 }
@@ -96,19 +113,24 @@ void setupRotaryButton() {
   pinMode(BUTTON_SW_PIN, INPUT_PULLUP);
 }
 
+void setupMoistureSensors() {
+  
+}
+
 void updateScreen() {
   screen.clearDisplay();
 
   // Header: Power Save Countdown
   long powerSaverCountdown = (DELAY_POWER_SAVER - (millis() - lastActivityTime)) / 1000;
   drawNumber(powerSaverCountdown, 0, 0);
+  //drawNumber(rotaryPosition, 0, 0);
 
   // Header: Battery
   double currentVolt = readVcc() / 1000.0;
   double batteryLevel = currentVolt - MIN_VOLT;
   uint8_t batteryPercent = (batteryLevel / (MAX_VOLT - MIN_VOLT)) * 100;
   drawBattery(batteryPercent, SCREEN_WIDTH - BATTERY_GAUGE_WIDTH, 0);
-  
+
 
   // Separator
   screen.drawFastHLine(0, 6, SCREEN_WIDTH, WHITE);
@@ -118,11 +140,14 @@ void updateScreen() {
     case PAGE_HOME:
       displayHomePage();
       break;
+    case PAGE_PLANT:
+      displayPlantPage();
+      break;
   }
   /*
-  screen.setCursor(0, 9);
-  screen.println(rotaryButtonValue);
-  screen.println(millis());
+    screen.setCursor(0, 9);
+    screen.println(rotaryButtonValue);
+    screen.println(millis());
   */
 
   // Update
@@ -153,12 +178,134 @@ void displayHomePage() {
   screen.drawFastVLine(iconX + 7, iconY + 5, 2, BLACK);
   screen.drawFastHLine(iconX + 6, iconY + 7, 2, BLACK);
   screen.drawFastHLine(iconX + 5, iconY + 8, 2, BLACK);
+
+  // Menu
+  uint8_t menuX = 20;
+  uint8_t menuY = 10;
+  for (uint8_t index = homePageMenuOffset; index < PLANT_COUNT; index++) {
+    screen.setCursor(menuX + 2, menuY + 2 + (index - homePageMenuOffset) * 12);
+    screen.print("Plante ");
+    screen.print(index + 1);
+    if (index == homePageMenuIndex) {
+      screen.fillRect(menuX, menuY + (index - homePageMenuOffset) * 12, SCREEN_WIDTH - menuX, 12, INVERSE);
+    }
+  }
+}
+
+void displayPlantPage() {
+  // Title
+  screen.setCursor(30, 10);
+  screen.print("Plante ");
+  screen.print(homePageMenuIndex + 1);
+
+  // Separator
+  screen.drawFastHLine(0, 20, SCREEN_WIDTH, WHITE);
+
+  // Moisture sensor
+  uint16_t moistureValue = analogRead(MOISTURE_SENSOR_1_PIN);
+  int8_t moisturePercent = (1 - ((moistureValue - MIN_MOISTURE_VALUE) / (MAX_MOISTURE_VALUE - MIN_MOISTURE_VALUE))) * 100;
+  if (moisturePercent < 0) {
+    moisturePercent = 0;
+  }
+  if (moisturePercent > 100) {
+    moisturePercent = 100;
+  }
+
+  // Moisture gauge
+  uint8_t startX = 0;
+  uint8_t startY = 25;
+  uint8_t width = 11;
+  uint8_t height = 26;
+  uint8_t gaugeHeight = ((height - 2) / 100) * moisturePercent;
+  screen.drawFastVLine(startX, startY, height, WHITE);
+  screen.drawFastVLine(startX + width - 1, startY, height, WHITE);
+  screen.drawFastHLine(startX, startY + height - 1, width, WHITE);
+  screen.fillRect(startX + 2, startY + height - gaugeHeight, width - 4, gaugeHeight, WHITE);
+
+  // Moisture percentage
+  drawNumber(moisturePercent, 1, 52);
+  drawNumber(moistureValue, 0, 59);
+
+  // Menu
+  uint8_t menuX = 20;
+  uint8_t menuY = 25;
+  for (uint8_t index = plantPageMenuOffset; index < plantPageMenuSize; index++) {
+    screen.setCursor(menuX + 2, menuY + 2 + (index - plantPageMenuOffset) * 12);
+    screen.print(plantPageMenu[index]);
+    if (index == plantPageMenuIndex) {
+      screen.fillRect(menuX, menuY + (index - plantPageMenuOffset) * 12, SCREEN_WIDTH - menuX, 12, INVERSE);
+    }
+  }
+}
+
+void onNext() {
+  wakeUp();
+
+  if (currentPage == PAGE_HOME) {
+    homePageMenuIndex++;
+    if (homePageMenuIndex > PLANT_COUNT - 1) {
+      homePageMenuIndex = 0;
+      homePageMenuOffset = 0;
+    } else if ((homePageMenuIndex - homePageMenuOffset) >= 4) {
+      homePageMenuOffset++;
+    }
+  } else if (currentPage == PAGE_PLANT) {
+    plantPageMenuIndex++;
+    if (plantPageMenuIndex > plantPageMenuSize - 1) {
+      plantPageMenuIndex = 0;
+      plantPageMenuOffset = 0;
+    } else if ((plantPageMenuIndex - plantPageMenuOffset) >= 3) {
+      plantPageMenuOffset++;
+    }
+  }
+}
+
+void onPrevious() {
+  wakeUp();
+
+  if (currentPage == PAGE_HOME) {
+    if (homePageMenuIndex == 0) {
+      homePageMenuIndex = PLANT_COUNT - 1;
+      homePageMenuOffset = PLANT_COUNT - 4;
+    } else {
+      homePageMenuIndex--;
+
+      if ((homePageMenuIndex - homePageMenuOffset) < 0) {
+        homePageMenuOffset--;
+      }
+    }
+  } else if (currentPage == PAGE_PLANT) {
+    if (plantPageMenuIndex == 0) {
+      plantPageMenuIndex = plantPageMenuSize - 1;
+      plantPageMenuOffset = plantPageMenuSize - 3;
+    } else {
+      plantPageMenuIndex--;
+
+      if ((plantPageMenuIndex - plantPageMenuOffset) < 0) {
+        plantPageMenuOffset--;
+      }
+    }
+  }
+}
+
+void onClick() {
+  wakeUp();
+
+  if (currentPage == PAGE_HOME) {
+    currentPage = PAGE_PLANT;
+    plantPageMenuIndex = 0;
+  } else if (currentPage == PAGE_PLANT) {
+    if (plantPageMenuIndex == 0) {
+      // Back
+      currentPage = PAGE_HOME;
+    }
+  }
 }
 
 void powerDown() {
   screen.clearDisplay();
   screen.display();
-  
+
   attachInterrupt(digitalPinToInterrupt(BUTTON_SW_PIN), wakeUp, LOW);
   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
   //LowPower.powerSave(SLEEP_FOREVER, ADC_OFF, BOD_OFF, TIMER2_ON);
@@ -249,7 +396,7 @@ void drawNumber(long value, uint8_t x, uint8_t y) {
     drawDigit(digit, x + offset, y);
     offset += 5;
   }
-  
+
   digit = value % 10;
   drawDigit(digit, x + offset, y);
 }
@@ -262,7 +409,7 @@ void drawBattery(uint8_t percent, uint8_t x, uint8_t y) {
   for (uint8_t offset = 0; offset < gauge; offset++) {
     screen.drawFastVLine(x + offset, y, 5, WHITE);
   }
-  
+
   screen.drawPixel(x + BATTERY_GAUGE_WIDTH - 1, y, BLACK);
   screen.drawPixel(x + BATTERY_GAUGE_WIDTH - 1, y + 4, BLACK);
   screen.drawPixel(x + BATTERY_GAUGE_WIDTH - 2, y + 1, WHITE);
